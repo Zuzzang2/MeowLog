d6b4d69a99cea94e1773ac8f57d0385d60ab300a
Zuzzang2 <mandy021021@gmail.com>
2025-02-21 18:51:02 +0900

    refactor: TimePicker 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import ProgressBar from "@/components/progress/ProgressBar";
 import { useCategoryStore } from "@/store/dataStore";
 import TimePicker from "@/components/time/TimePicker";
 import Link from "next/link";
+import TimePickerList from "@/components/time/TimePickerList";
 
 moment.locale("ko"); // 기본 로케일을 한국어로 설정
 
@@ -17,27 +18,6 @@ export default function Main() {
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
   const { categoryList } = useCategoryStore();
-
-  // ✅ 각 카테고리별로 여러 개의 TimePicker를 관리하는 상태. timePickersByCategory= TimePicker들의 ID(숫자 배열)를 저장하는 객체 상태
-  const [timePickersByCategory, setTimePickersByCategory] = useState<{
-    [key: string]: number[]; //key: 카테고리 이름, value: 타임피커의 id 배열
-  }>({}); //초기상태를 {}빈객체로 설정
-
-  // ✅ 특정 카테고리의 TimePicker를 추가하는 함수 -> 기존 timePickersByCategory를 유지하면서, 새로운 타임피커 ID를 추가
-  const addTimePicker = (category: string) => {
-    setTimePickersByCategory((prev) => ({
-      ...prev, //기존 상태 복사
-      [category]: [...(prev[category] || []), Date.now()], // prev[category] || [] → 기존에 추가된 타임피커가 없으면 빈 배열을 사용. Date.now() → 고유한 ID를 생성하여 TimePicker 추가
-    }));
-  };
-
-  // ✅ 특정 TimePicker를 삭제하는 함수. 특정 타임피커의 취소 버튼 클릭 시 해당 타임피커의 id를 찾아서 제거. filter()를 사용하여 선택한 ID를 제외한 배열을 다시 상태로 설정
-  const removeTimePicker = (category: string, id: number) => {
-    setTimePickersByCategory((prev) => ({
-      ...prev,
-      [category]: prev[category].filter((pickerId) => pickerId !== id), // 해당 id만 삭제
-    }));
-  };
 
   //✅ 모달 외부 클릭 감지 (메뉴)
   useEffect(() => {
@@ -111,32 +91,8 @@ export default function Main() {
           </div>
         </div>
 
-        {categoryList.map((category, index) => (
-          <div className="flex flex-col p-4">
-            <div className="my-2">
-              <a className="h-[200px] bg-blue-500 text-white rounded">
-                {category}
-              </a>
-            </div>
-            <div>
-              {/* ✅ 추가 버튼 */}
-              <button
-                className="border mt-2 p-2"
-                onClick={() => addTimePicker(category)}
-              >
-                추가
-              </button>
-            </div>
-
-            {/* ✅ 추가된 TimePicker 목록. timePickersByCategory[category] → 해당 카테고리의 모든 타임피커 ID 배열을 가져옴 -> map()을 사용하여 해당 ID를 가진 타임피커 컴포넌트를 동적으로 생성 */}
-            {timePickersByCategory[category]?.map((id) => (
-              <TimePicker
-                key={id}
-                onCancel={() => removeTimePicker(category, id)}
-              />
-            ))}
-          </div>
-        ))}
+        {/* ✅ 타임피커 리스트 컴포넌트만 렌더링 */}
+        <TimePickerList categoryList={categoryList} />
       </div>
     </main>
   );
