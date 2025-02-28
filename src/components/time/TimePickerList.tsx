de586b3d740abb34763615e518f3e596ce35a11f
Zuzzang2 <mandy021021@gmail.com>
2025-02-28 19:04:44 +0900

    feat: 카테고리 props 전송으로 단위 표시 추가

--- src/components/time/TimePickerList.tsx
+++ src/components/time/TimePickerList.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import TimePicker from "./TimePicker";
-import { useTimeStore } from "@/store/timeStore";
-import { useCategoryStore } from "@/store/categoryStore";
+import { useTimeStore } from "@/store/useTimeStore";
+import { useCategoryStore } from "@/store/useCategoryStore";
+import { useUnitStore } from "@/store/useUnitStore";
 
 export default function TimePickerList() {
   const { categoryList } = useCategoryStore();
@@ -19,14 +20,16 @@ export default function TimePickerList() {
             </span>
           </div>
           <div>
-            {/* ✅ 타임피커 목록 렌더링. timePickersByCategory[category] → 해당 카테고리의 모든 타임피커 ID 배열을 가져옴 -> map()을 사용하여 해당 ID를 가진 타임피커 컴포넌트를 동적으로 생성*/}
+            {/* 타임피커 목록 렌더링. timePickersByCategory[category] → 해당 카테고리의 모든 타임피커 ID 배열을 가져옴 -> map()을 사용하여 해당 ID를 가진 타임피커 컴포넌트를 동적으로 생성*/}
             {timePickersByCategory[category]?.map((id) => (
               <TimePicker
                 key={id}
+                category={category}
                 onCancel={() => removeTimePicker(category, id)}
               />
             ))}
-            {/* ✅ 추가 버튼 */}
+
+            {/* 추가 버튼 */}
             <button
               className="border mt-2 p-2"
               onClick={() => addTimePicker(category)}
